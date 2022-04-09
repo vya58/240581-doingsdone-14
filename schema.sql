7254a75c31747aa1d66590c2349f38190639f432
vya58 <vya58@yandex.ru>
2022-04-09 14:09:46 +0300

    Добавил кодировку utf-8

--- schema.sql
+++ schema.sql
@@ -1,4 +1,6 @@
-CREATE DATABASE doings_done;
+CREATE DATABASE doings_done
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE utf8_general_ci;
 
 CREATE TABLE users (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
